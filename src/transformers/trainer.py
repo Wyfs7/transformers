ec82b584a950a23a30b3042a61365d418e315d83
uggggg <951548039@qq.com>
2024-05-25 17:51:45 +0800

    1

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -216,8 +216,9 @@ if TYPE_CHECKING:
     import optuna
 
 
-logger = logging.get_logger(__name__)
+#logger = logging.get_logger(__name__)
 #logger = logging.get_logger("DeepSpeed")
+logger = logging.get_logger("transformers")
 
 # Name of the files used for checkpointing
 TRAINING_ARGS_NAME = "training_args.bin"
@@ -1908,7 +1909,9 @@ class Trainer:
                     model.zero_grad()
                     self.state.global_step += 1
                     self.state.epoch = epoch + (step + 1 + steps_skipped) / steps_in_epoch
-                    self.control = self.callback_handler.on_step_end(args, self.state, self.control)
+                    #import pdb;pdb.set_trace()
+                    logger.info(f'set global step{step}')
+                    #self.control = self.callback_handler.on_step_end(args, self.state, self.control)
 
                     self._maybe_log_save_evaluate(tr_loss, model, trial, epoch, ignore_keys_for_eval)
                 else:
